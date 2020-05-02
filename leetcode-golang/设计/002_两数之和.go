1a4755d55a5e6b86190a6a0ce0fa95fd206d493f
zhan <grianchan@gmail.com>
2020-05-02 23:22:41 +0800

    add

---
+++ leetcode-golang/设计/002_两数之和.go
@@ -0,0 +1,45 @@
+package 设计
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	up := 0
+	c1 := l1
+	c2 := l2
+	l3 := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	c3 := l3
+	for c1 != nil || c2 != nil {
+		v := 0
+		if c1 == nil {
+			v = c2.Val + up
+			c2 = c2.Next
+		} else if c2 == nil {
+			v = c1.Val + up
+			c1 = c1.Next
+		} else {
+			v = c1.Val + c2.Val + up
+			c1 = c1.Next
+			c2 = c2.Next
+		}
+		up = v / 10
+		v = v % 10
+		c3.Next = &ListNode{
+			Val:  v % 10,
+			Next: nil,
+		}
+		c3 = c3.Next
+	}
+	if up == 1 {
+		c3.Next = &ListNode{
+			Val:  1,
+			Next: nil,
+		}
+	}
+	return l3.Next
+}
---
+++ leetcode-golang/设计/002_两数之和_test.go
@@ -0,0 +1,28 @@
+package 设计
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	l1 := &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 5,
+		Next: &ListNode{
+			Val: 6,
+			Next: &ListNode{
+				Val:  7,
+				Next: nil,
+			},
+		},
+	}
+	l3 := addTwoNumbers(l1, l2)
+
+}
