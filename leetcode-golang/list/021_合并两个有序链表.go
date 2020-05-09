24d7a3e0dc058ecad842d0cefc41fef074095f92
zhan <grianchan@gmail.com>
2020-05-09 11:54:41 +0800

    add

---
+++ leetcode-golang/list/021_合并两个有序链表.go
@@ -0,0 +1,38 @@
+package list
+
+//将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//示例：
+//
+//输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	cur := res
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+	if l1 != nil {
+		cur.Next = l1
+	}
+	if l2 != nil {
+		cur.Next = l2
+	}
+	return res.Next
+}
