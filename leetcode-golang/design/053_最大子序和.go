f74842de174bc11aad9bac6dba550f6d76c8b4cf
zhan <grianchan@gmail.com>
2020-05-10 23:24:27 +0800

    add

---
+++ leetcode-golang/design/053_最大子序和.go
@@ -0,0 +1,29 @@
+package design
+
+import "math"
+
+// 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+//示例:
+//
+//输入: [-2,1,-3,4,-1,2,1,-5,4],
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+//进阶:
+//
+//如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/maximum-subarray
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func maxSubArray(nums []int) int {
+	for k, v := range nums {
+		if k < 1 {
+			continue
+		} else {
+			nums[k] = int(math.Max(float64(v), float64(nums[k-1]+v)))
+		}
+	}
+	return nums[len(nums)-1]
+}
