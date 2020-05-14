049dd28d236e3653235e115912e5d1b98f91bd38
zhan <grianchan@gmail.com>
2020-05-14 16:38:29 +0800

    add

---
+++ leetcode-golang/search/035_搜索插入位置.go
@@ -0,0 +1,43 @@
+package search
+
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+//你可以假设数组中无重复元素。
+//
+//示例 1:
+//
+//输入: [1,3,5,6], 5
+//输出: 2
+//示例 2:
+//
+//输入: [1,3,5,6], 2
+//输出: 1
+//示例 3:
+//
+//输入: [1,3,5,6], 7
+//输出: 4
+//示例 4:
+//
+//输入: [1,3,5,6], 0
+//输出: 0
+
+func searchInsert(nums []int, target int) int {
+	// 二分法找位置
+	l := 0
+	r := len(nums) - 1
+	for l < r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		} else if nums[m] > target {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	if target > nums[l] {
+		return l + 1
+	} else {
+		return l
+	}
+}
