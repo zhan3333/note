40933c0505d3b59887b3c67d99d3fbfff736dd6c
zhan <grianchan@gmail.com>
2020-05-17 16:37:04 +0800

    add

---
+++ leetcode-golang/string/058_最后一个单词的长度.go
@@ -0,0 +1,26 @@
+package string
+
+import "strings"
+
+// 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+//
+//如果不存在最后一个单词，请返回 0 。
+//
+//说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/length-of-last-word
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func lengthOfLastWord(s string) int {
+	s = strings.Trim(s, " ")
+	count := 0
+	for _, c := range s {
+		if string(c) == " " {
+			count = 0
+		} else {
+			count++
+		}
+	}
+	return count
+}
