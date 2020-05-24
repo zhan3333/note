2165ccfc06f65dd1222a380e6a8c2d6651755064
zhan <grianchan@gmail.com>
2020-05-24 14:09:52 +0800

    add

---
+++ leetcode-golang/array/088_合并两个有序数组.go
@@ -0,0 +1,17 @@
+package array
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for m > 0 && n > 0 {
+		if nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+	for n > 0 {
+		nums1[n-1] = nums2[n-1]
+		n--
+	}
+}
