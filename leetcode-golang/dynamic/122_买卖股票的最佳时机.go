ff0d3dc601a3121017be77d041abd497a8bae35d
zhan <grianchan@gmail.com>
2020-06-03 10:05:59 +0800

    add

--- leetcode-golang/dynamic/122_买卖股票的最佳时机.go
+++ leetcode-golang/dynamic/122_买卖股票的最佳时机.go
@@ -18,3 +18,12 @@ func maxProfit2(prices []int) int {
 	}
 	return sum
 }
+func maxProfit3(prices []int) int {
+	sum := 0
+	for i := 1; i < len(prices); i++ {
+		if t := prices[i] - prices[i-1]; t > 0 {
+			sum += t
+		}
+	}
+	return sum
+}
