b3a42408ba862f54c321cf60f00d77f57e5f338c
zhan <grianchan@gmail.com>
2020-07-21 18:38:51 +0800

    add

--- temp/test.go
+++ temp/test.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1,2}
+	arr := []int{1, 2}
 	fmt.Printf("%+v \n", arr[0:0])
 	fmt.Printf("%+v \n", arr[1:])
 	fmt.Printf("%+v \n", arr[:])
+	fmt.Printf("%+v", fmt.Errorf("test"))
 }
