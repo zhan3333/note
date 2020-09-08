6a78bad4d52defbb04e7edddcb42ff1589f850d0
zhan <grianchan@gmail.com>
2020-09-08 20:23:46 +0800

    add

---
+++ leetcode/editor/cn/[55]跳跃游戏.go
@@ -0,0 +1,46 @@
+package leetcode_golang
+
+//给定一个非负整数数组，你最初位于数组的第一个位置。
+//
+// 数组中的每个元素代表你在该位置可以跳跃的最大长度。 
+//
+// 判断你是否能够到达最后一个位置。 
+//
+// 示例 1: 
+//
+// 输入: [2,3,1,1,4]
+//输出: true
+//解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+// 
+//
+// 示例 2: 
+//
+// 输入: [3,2,1,0,4]
+//输出: false
+//解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+// 
+// Related Topics 贪心算法 数组
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func canJump(nums []int) bool {
+	if len(nums) <= 1 {
+		return true
+	}
+	nums[0] = -nums[0]
+	for i, n := range nums {
+		if n >= 0 {
+			continue
+		}
+		for j := 1; j <= -n; j++ {
+			if i+j == len(nums)-1 {
+				return true
+			}
+			if nums[i+j] > 0 {
+				nums[i+j] = -nums[i+j]
+			}
+		}
+	}
+	return false
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
